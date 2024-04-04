a2cd3fae5014b5a0a883da44ffdf00a22734f9ec
VeereshaS431 <veereshveerus2257@gmail.com>
2024-04-04 14:33:13 +0530

    newaxies fetchData

---
+++ src/axiesFetchData/productList.js
@@ -0,0 +1,103 @@
+import { Component } from "react";
+import axios from 'axios';
+import "./productsstyle.css"
+
+
+class Products extends Component {
+
+    state = {
+        items: []
+    }
+
+    componentDidMount() {
+        this.fecthData()
+    }
+
+    fecthData = async () => {
+        const result = await axios("https://dummyjson.com/products")
+
+        this.setState({
+            items: result.data.products
+        })
+
+    }
+
+
+    Delete=(id)=>{
+        const newItems=this.state.items.filter((val)=>{
+            return val.id!=id
+        })
+
+        this.setState({
+            items:newItems
+        })
+    }
+
+    Update=(id)=>{
+        const newItems=this.state.items.map((val)=>{
+            if(val.id==id){
+                return ({
+                    "id": 3,
+                    "title": "Mens Cotton Jacket",
+                    "price": 55.99,
+                    "description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+                    "category": "men's clothing",
+                    "thumbnail": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+                    "rating": {
+                        "rate": 4.7,
+                        "count": 500
+                    }
+                })
+            }
+            else{
+                return val
+            }
+           
+        })
+
+        this.setState({
+            items:newItems
+        })
+    }
+
+
+
+
+    render() {
+        console.log(this.state.items)
+        return (
+            <div>
+                {
+
+                    this.state.items.length > 0
+                        ?
+                        <div className="main">
+                            {
+                                this.state.items.map((val) => {
+                                    console.log(val)
+                                    return (
+                                        <div className="cards">
+                                            <img src={val.thumbnail} alt={val.title}/>
+                                            <h2>{val.title}</h2>
+                                            {/* <p>{val.description}</p> */}
+                                            <h6>₹ {val.price}</h6>
+                                            <div className="btn">
+                                                <button onClick={()=>this.Delete(val.id)}>Delete</button>
+                                                <button onClick={()=>this.Update(val.id)}>Update</button>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                        :
+                        
+                        <h1>loading</h1>
+                        
+                }
+    
+            </div>
+        )
+    }
+}
+export default Products;
