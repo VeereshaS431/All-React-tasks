b505b0a7b05603ce416be8bce90b6476aa9448bf
VeereshaS431 <veereshveerus2257@gmail.com>
2024-04-05 17:49:49 +0530

    new file

--- src/axiesFetchData/productList.js
+++ src/axiesFetchData/productList.js
@@ -13,55 +13,65 @@ class Products extends Component {
 
     componentDidMount() {
         this.fecthData()
+
     }
 
     fecthData = async () => {
         const result = await axios("https://dummyjson.com/products")
 
+
+        const result1 = result.data.products.map((val) => {
+            const data = { ...val, count: 1, totalPrice: val.price }
+            return data
+        })
+
         this.setState({
-            items: result.data.products
+            items: result1
         })
 
     }
 
 
-    Delete=(id)=>{
-        const newItems=this.state.items.filter((val)=>{
-            return val.id!=id
-        })
-
-        this.setState({
-            items:newItems
-        })
-    }
-
-    Update=(id)=>{
-        const newItems=this.state.items.map((val)=>{
-            if(val.id==id){
-                return ({
-                    "id": 3,
-                    "title": "Mens Cotton Jacket",
-                    "price": 55.99,
-                    "description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
-                    "category": "men's clothing",
-                    "images": ["https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"],
-                    "rating": {
-                        "rate": 4.7,
-                        "count": 500
-                    }
-                })
-            }
-            else{
+    increment = (id) => {
+        const newItems = this.state.items.map((val) => {
+            if (id == val.id) {
+                val.count += 1
+                val.totalPrice = val.price * val.count
                 return val
             }
-           
+            else {
+                return val
+            }
         })
 
         this.setState({
-            items:newItems
+            items: newItems
         })
     }
 
+    decrement = (id) => {
+        const newItems = this.state.items.map((val) => {
+            if (id == val.id) {
+                if (val.count > 0) {
+                    val.count -= 1
+                    val.totalPrice = val.price * val.count
+
+                    return val
+                }
+                else {
+                    return val
+                }
+            }
+            else {
+                return val
+            }
+        })
+
+        this.setState({
+            items: newItems
+        })
+
+    }
 
 
 
@@ -69,34 +79,52 @@ class Products extends Component {
         console.log(this.state.items)
         return (
             <div className="center">
+
                 {
 
                     this.state.items.length > 0
                         ?
-                        <div className="main">
-                            {
-                                this.state.items.map((val) => {
-                                    console.log(val)
-                                    return (
-                                        <div className="cards">
-                                            <img src={val.images[0]} alt={val.title}/>
-                                            <h2>{val.title}</h2>
-                                            {/* <p>{val.description}</p> */}
-                                            <h6>₹ {val.price}</h6>
-                                            <div className="btn1">
-                                                <button onClick={()=>this.Delete(val.id)}>Delete</button>
-                                                <button onClick={()=>this.Update(val.id)}>Update</button>
+                        <div className="container">
+                            <div className="totalsum"><h1>Total Sum: ₹ <span>
+                                {
+                                    this.state.items.reduce((acc, val) => {
+                                        return acc + val.totalPrice
+                                    }, 0)
+                                }
+                            </span>
+                            </h1>
+                            </div>
+                            <div className="main">
+                                {
+                                    this.state.items.map((val) => {
+                                        console.log(val)
+                                        return (
+                                            <div className="cards">
+                                                <img src={val.images[0]} alt={val.title} />
+                                                <h2>{val.title}</h2>
+                                                <h6>₹ {val.price}</h6>
+
+                                                <div className="btn1">
+                                                    <button onClick={() => this.decrement(val.id)}><i class="fa-solid fa-minus"></i></button>
+                                                    <h4>{val.count}</h4>
+                                                    <button onClick={() => this.increment(val.id)}><i class="fa-solid fa-plus"></i></button>
+                                                </div>
+
+                                                <div>
+                                                    <h4>Total: ₹ <span>{val.totalPrice}</span></h4>
+                                                </div>
+
                                             </div>
-                                        </div>
-                                    )
-                                })
-                            }
+                                        )
+                                    })
+                                }
+                            </div>
                         </div>
                         :
-          
-                        <CustomSpinner/>
+
+                        <CustomSpinner />
                 }
-    
+
             </div>
         )
     }
